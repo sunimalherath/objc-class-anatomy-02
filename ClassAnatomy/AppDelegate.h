751de2459cc9e3f9c77680e54c4c3e1a8ce7671c
sunimalherath <sunimaldv@gmail.com>
2020-09-20 19:39:12 +1000

    Initial Commit

---
+++ ClassAnatomy/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  ClassAnatomy
+//
+//  Created by Sunimal Herath on 20/9/20.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
